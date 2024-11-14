2f94159dd1b58cf6eb3e41f079c40383b5527491
Zan-Kir <murilo4santos@hotmail.com>
2024-11-14 20:53:57 -0300

    feat: vercel/node

--- dist/index.js
+++ dist/index.js
@@ -11,11 +11,12 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
-    origin: `${process.env.VITE_FRONTEND_URL}`
+    origin: `${process.env.VITE_FRONTEND_URL}` // Sua URL de frontend
 }));
 (0, database_1.default)();
 app.use(express_1.default.json());
 app.use('/api/auth', userRoutes_1.default);
-// eslint-disable-next-line no-undef
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Exportando a função serverless que será chamada pelo Vercel
+exports.default = (req, res) => {
+    app(req, res); // Express processa a requisição
+};
--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,22 @@ import cors from 'cors';
 import connectDB from './config/database';
 import authRoutes from './routes/userRoutes';
 import dotenv from 'dotenv';
+import { VercelRequest, VercelResponse } from '@vercel/node';
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors({
-    origin: `${process.env.VITE_FRONTEND_URL}`
+  origin: `${process.env.VITE_FRONTEND_URL}` 
 }));
 
 connectDB();
 
 app.use(express.json());
-
 app.use('/api/auth', authRoutes);
 
-// eslint-disable-next-line no-undef
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+export default (req: VercelRequest, res: VercelResponse) => {
+  app(req, res);
+};
